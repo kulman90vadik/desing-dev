a7e16e3f243b5ec917d4f0854883ffb5a8868c77
Vadim Kulman <vadim.kulman90@mail.ru>
2021-06-19 00:58:39 +0300

    respoonsiv header, home-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function(){
 
+    $('.header__button').on('click', function(){
+        $('.menu').toggleClass('menu--active');
+        $('.header__items').toggleClass('header__items--active');
+    });
+
     $('.about-slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><svg class="about-slider__icon"><use xlink:href="images/sprite.svg#prev"></use></svg></button>',
         nextArrow: '<button type="button" class="slick-next"><svg class="about-slider__icon"><use xlink:href="images/sprite.svg#next"></use></svg></button>',
@@ -28,28 +33,28 @@ $(function(){
 
     $('.menu__link, .logo').on('click', function (event) {
             event.preventDefault();
-            var id = $(this).attr('href'),
-            top = $(id).offset().top;
+            var id = $(this).attr('href');
+            var top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 1500);
+
+            // !
+            $('.menu').removeClass('menu--active');
+            $('.header__items').removeClass('header__items--active');
     });
 
 
-    var wow = new WOW(
-        {
-          boxClass:     'wow',     
-          animateClass: 'animate__animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
-          live:         true,       // act on asynchronously loaded content (default is true)
-          callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-          },
-          scrollContainer: null,    // optional scroll container selector, otherwise use window,
-          resetAnimation: true,     // reset animation on end (default is true)
-        }
-      );
-      wow.init();
+    var wow = new WOW({
+        boxClass: 'wow',     
+            animateClass: 'animate__animated', 
+            offset: 0,         
+            mobile: true,      
+            live: true,     
+        callback: function(box) {
+        },
+        scrollContainer: null,    
+        resetAnimation: true,     
+    });
+    wow.init();
 
 
     var containerEl1 = document.querySelector('[data-ref="container-1"]');
